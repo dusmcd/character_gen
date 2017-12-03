29f156c9ee83c7fd375b942183d3fdd9f87ab062
dusmcd <dustinmcdowell1989@gmail.com>
2017-12-03 00:05:37 +0000

    comment out edit routes

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ app.set('view engine', 'ejs'),
 app.use(express.static(__dirname + '/public'));      
 
 //connect to db
-const promise = mongoose.connect("mongodb://localhost/character_gen", {
+// const promise = mongoose.connect("mongodb://localhost/character_gen", {
+//     useMongoClient: true
+// });
+
+const promise = mongoose.connect('mongodb://dustin:nitsud181@ds125716.mlab.com:25716/character_gen', {
     useMongoClient: true
 });
 
@@ -41,38 +45,37 @@ app.get('/generating', function(req, res) {
         }
     });
     // res.send('Hello, there!'),
-    React = require('react');
 });
 
 //edit traits
-app.get('/traits/:id/edit', function(req, res) {
-    Trait.findById(req.params.id, function(err, trait) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('edit', {trait: trait});
-        }
-    });
-});
+// app.get('/traits/:id/edit', function(req, res) {
+//     Trait.findById(req.params.id, function(err, trait) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             res.render('edit', {trait: trait});
+//         }
+//     });
+// });
 
-app.put('/traits/:id', function(req, res) {
-    Trait.findById(req.params.id, function(err, traitDoc) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        if (req.body.positiveTrait) {
-            traitDoc.positiveTraits.push(req.body.positiveTrait);
-        }
-        if (req.body.negativeTrait) {
-            traitDoc.negativeTraits.push(req.body.negativeTrait);
-        }
-        if (req.body.characterType) {
-            traitDoc.characterTypes.push(req.body.characterType);
-        }
-        traitDoc.save();
-    });    
-});
+// app.put('/traits/:id', function(req, res) {
+//     Trait.findById(req.params.id, function(err, traitDoc) {
+//         if (err) {
+//             console.log(err);
+//             return;
+//         }
+//         if (req.body.positiveTrait) {
+//             traitDoc.positiveTraits.push(req.body.positiveTrait);
+//         }
+//         if (req.body.negativeTrait) {
+//             traitDoc.negativeTraits.push(req.body.negativeTrait);
+//         }
+//         if (req.body.characterType) {
+//             traitDoc.characterTypes.push(req.body.characterType);
+//         }
+//         traitDoc.save();
+//     });    
+// });
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("The server is listening");
